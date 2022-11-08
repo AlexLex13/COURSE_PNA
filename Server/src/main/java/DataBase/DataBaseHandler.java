1e7df08102a6b1bc3f2ede6b6650ef92273a76a0
Alexey <suxovarov.alexey@gmail.com>
2022-11-08 16:42:03 +0300

    add DataBaseHandler

---
+++ Server/src/main/java/DataBase/DataBaseHandler.java
@@ -0,0 +1,36 @@
+package DataBase;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class DataBaseHandler {
+    public static void main(String args[]) {
+        try {
+            var prop = getPropertiesFromConfig();
+            var str = prop.getProperty("connectionString");
+            Connection c = DriverManager
+                    .getConnection(str,
+                            "postgres", "2003ktrf10");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+
+    private static Properties getPropertiesFromConfig() throws IOException {
+
+        var properties = new Properties();
+        String propFileName = "Server/src/main/resources/config.properties";
+        var inputStream = new FileInputStream(propFileName);
+        if (inputStream == null)
+            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+        properties.load(inputStream);
+        return properties;
+    }
+}
