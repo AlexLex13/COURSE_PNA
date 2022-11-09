b0975bcd493bf5d17ced332a26cf35f5e7a3e3c7
Alexey <suxovarov.alexey@gmail.com>
2022-11-09 14:46:11 +0300

    add execute SQL

--- Server/src/main/java/DataBase/DataBaseHandler.java
+++ Server/src/main/java/DataBase/DataBaseHandler.java
@@ -3,24 +3,27 @@ package DataBase;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.Properties;
 
 public class DataBaseHandler {
-    public static void main(String args[]) {
+
+    public static Connection getDBConnection() {
+        Connection dbConnection = null;
+        
         try {
             var prop = getPropertiesFromConfig();
-            var str = prop.getProperty("connectionString");
-            Connection c = DriverManager
-                    .getConnection(str,
-                            "postgres", "2003ktrf10");
+            var connectionString = prop.getProperty("connectionString");
+            var username = prop.getProperty("user");
+            var password = prop.getProperty("password");
+            dbConnection = DriverManager.getConnection(connectionString, username, password);
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName()+": "+e.getMessage());
             System.exit(0);
         }
         System.out.println("Opened database successfully");
+        return dbConnection;
     }
 
     private static Properties getPropertiesFromConfig() throws IOException {
@@ -32,5 +35,18 @@ public class DataBaseHandler {
             throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
         properties.load(inputStream);
         return properties;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        ResultSet resSet = null;
+        try {
+            PreparedStatement prSt = getDBConnection().prepareStatement("SELECT * FROM \"Admin\" ORDER BY id ASC ");
+            resSet = prSt.executeQuery();
+            resSet.next();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(resSet.getString("password"));
     }
 }
