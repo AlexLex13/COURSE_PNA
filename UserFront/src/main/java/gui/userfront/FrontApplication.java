5df6f904a4197aced4110f8ebd64668e0c21a861
Alexey <suxovarov.alexey@gmail.com>
2022-11-10 20:20:50 +0300

    new UserFront

---
+++ UserFront/src/main/java/gui/userfront/FrontApplication.java
@@ -0,0 +1,38 @@
+package gui.userfront;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class FrontApplication extends Application {
+    static Socket socket;
+    static DataInputStream inStream;
+    static DataOutputStream outStream;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(FrontApplication.class.getResource("front-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) throws IOException {
+        socket = new Socket("127.0.0.1", 8888);
+        inStream = new DataInputStream(socket.getInputStream());
+        outStream = new DataOutputStream(socket.getOutputStream());
+
+        launch(args);
+
+        inStream.close();
+        outStream.close();
+        socket.close();
+    }
+}
---
+++ UserFront/src/main/java/gui/userfront/FrontController.java
@@ -0,0 +1,45 @@
+package gui.userfront;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import static gui.userfront.FrontApplication.*;
+
+public class FrontController {
+    @FXML
+    private Label authorization;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Button entryButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button registrationButton;
+
+    @FXML
+    protected void onEntryButtonClick() {
+        String clientMessage, serverMessage;
+        try {
+            clientMessage ="authorization " + loginField.getText()+ " " + passwordField.getText();
+            outStream.writeUTF(clientMessage);
+            outStream.flush();
+            serverMessage = inStream.readUTF();
+            authorization.setText(serverMessage);
+//            if (!serverMessage.equals("Error!"))
+//                new InitialWindow(socket, inStream, outStream).display();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
